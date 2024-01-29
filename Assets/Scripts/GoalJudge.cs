9efcd12a59c0156f9c244f1f4b306bdf8cf9ed24
Eichansan <eisuke0327@icloud.com>
2024-01-29 13:24:27 +0900

    準完成版

--- Assets/Scripts/GoalJudge.cs
+++ Assets/Scripts/GoalJudge.cs
@@ -8,12 +8,28 @@ public class GoalJudge : MonoBehaviour
 {
     [SerializeField] GameSEManager gameSEManager;
     [SerializeField] GameObject resultObjs;
-    [SerializeField] TMP_Text resultText;
+    [SerializeField] GameObject titleButton;
+    public List<TMP_Text> resultText = new List<TMP_Text>();
+    int rank = 0;
     bool firstGoaled;
     private void Start() 
     {
-        resultObjs.SetActive(false);   
+        resultObjs.SetActive(false);  
+        titleButton.SetActive(false); 
     }
+    // void OnTriggerEnter(Collider collider)
+    // {
+    //     if (collider.gameObject.tag == "horse")
+    //     {
+    //         if (!firstGoaled)
+    //         {
+    //             StartCoroutine(gameSEManager.GoalSE());
+    //             firstGoaled = true;
+    //             resultText.text = collider.gameObject.GetComponent<Horse>().GetHorseText();
+    //             resultObjs.SetActive(true);
+    //         }
+    //     }
+    // }
     void OnTriggerEnter(Collider collider)
     {
         if (collider.gameObject.tag == "horse")
@@ -22,9 +38,11 @@ public class GoalJudge : MonoBehaviour
             {
                 StartCoroutine(gameSEManager.GoalSE());
                 firstGoaled = true;
-                resultText.text = collider.gameObject.GetComponent<Horse>().GetHorseText();
                 resultObjs.SetActive(true);
+                titleButton.SetActive(true);
             }
+            resultText[rank].text = $"{rank+1}. " + collider.gameObject.GetComponent<Horse>().GetHorseText();
+            rank++;
         }
     }
 
